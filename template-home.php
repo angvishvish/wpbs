6d97b588c73f049d006c6057d534e2c9fa0dea51
angvishvish <angvish@promobitech.com>
2015-03-26 00:44:55 +0530

    adding template for the homepage

--- template-home.php
+++ template-home.php
@@ -10,8 +10,22 @@
 get_header(); ?>
 
 <div class="container">
-  <div class="row default-padding">
-    
+  <div class="row">
+    <div class="col-md-8">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <article <?php post_class(); ?> class="wpbs-post">
+        <h3 class="wpbs-title">
+          <a href="<?php the_permalink(); ?>" class="wpbs-title"><?php the_title(); ?></a>
+        </h3>
+        <p class="wpbs-desc">
+          <?php the_content( __("Read more &raquo;","wpbs") ); ?>
+        </p>
+      </article>
+      <?php endwhile; endif; ?>
+    </div>
+    <div class="col-md-4">
+      <?php get_sidebar(); ?>
+    </div>
   </div>
 </div>
 
